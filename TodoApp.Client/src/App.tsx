96eded62fcbfbe9bf37c8de97deaefdf315d4ff8
jason <jason@DESKTOP-JEH4CSS>
2025-12-30 15:29:52 -0500

    minor change

--- TodoApp.Client/src/App.tsx
+++ TodoApp.Client/src/App.tsx
@@ -462,7 +462,7 @@ function App() {
                 disabled={loading || input.trim() === ''}
                 className="px-8 py-2.5 bg-blue-600 text-white font-semibold rounded-xl hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500/60 focus:ring-offset-2 focus:ring-offset-slate-800 transition-all duration-200 active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-600"
               >
-                {loading ? 'Adding...' : 'Add'}
+                Add
               </button>
             </div>
           </div>
