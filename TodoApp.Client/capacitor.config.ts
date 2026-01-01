bbe38e159c7b9388610f4107f46b7716d608638c
jason <jason@DESKTOP-JEH4CSS>
2025-12-31 23:09:01 -0500

    android

---
+++ TodoApp.Client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.todoapp.client',
+  appName: 'TodoApp',
+  webDir: 'dist'
+};
+
+export default config;
